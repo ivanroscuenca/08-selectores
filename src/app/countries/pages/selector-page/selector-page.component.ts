ea6a7d9a84bcd1d28dceb385e2fea4456c7eb853
ivanroscuenca <ivanros@protonmail.com>
2023-09-08 16:07:40 +0200

    video263

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
 })
 export class SelectorPageComponent {
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Region } from '../interfaces/country.interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
-
-  constructor() { }
+  //_ significa una convención utilizada para indicar que la variable o propiedad es privada y no debería ser accedida o modificada directamente desde fuera de la clase
+  private _regions: Region[] = [
+    Region.Africa,
+    Region.Americas,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania,
+  ];
+  get regions(): Region[] {
+    return [...this._regions];
+  }
+  constructor() {}
 }
