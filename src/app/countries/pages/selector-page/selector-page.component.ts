8cc9e59d92562bd931010fa8df6a960261efc95f
ivanroscuenca <ivanros@protonmail.com>
2023-09-08 13:32:38 +0200

    video261

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
 })
-export class SelectorPageComponent {}
+export class SelectorPageComponent {
+  constructor(private fb: FormBuilder) {}
+
+  public myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required],
+  });
+}
