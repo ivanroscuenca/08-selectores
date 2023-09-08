06c515d1716780346574cea8a38cf5949d42791b
ivanroscuenca <ivanros@protonmail.com>
2023-09-08 13:07:43 +0200

    video260

---
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+})
+export class SelectorPageComponent {}
