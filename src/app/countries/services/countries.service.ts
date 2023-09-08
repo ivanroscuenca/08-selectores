82b5eddeca4faca68cdf751ded194aeefb718080
ivanroscuenca <ivanros@protonmail.com>
2023-09-08 17:20:57 +0200

    video264

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interfaces';
+import { Region, SmallCountry } from '../interfaces/country.interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
+  constructor() {}
   //_ significa una convención utilizada para indicar que la variable o propiedad es privada y no debería ser accedida o modificada directamente desde fuera de la clase
   private _regions: Region[] = [
     Region.Africa,
@@ -16,5 +17,8 @@ export class CountriesService {
   get regions(): Region[] {
     return [...this._regions];
   }
-  constructor() {}
+
+  getCountriesByRegion(region: Region): SmallCountry {
+    return [];
+  }
 }
