e81090f33e9a67c8d51e6c06f75f6ddc37207ee9
ivanroscuenca <ivanros@protonmail.com>
2023-09-10 13:01:56 +0200

    video267

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -23,6 +23,7 @@ export class SelectorPageComponent implements OnInit {
     this.myForm
       .get('region')!
       .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
         switchMap((region) =>
           this.countriesService.getCountriesByRegion(region)
         )
